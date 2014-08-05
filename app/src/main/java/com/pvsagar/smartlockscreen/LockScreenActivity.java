43fe538805dd35d2f3a527a893a5bd4deb1d9e01
aravindsagar <aravindsuhruth@gmail.com>
2014-08-05 19:12:05 +0530

    Made current wallpaper as the background of app.

--- app/src/main/java/com/pvsagar/smartlockscreen/LockScreenActivity.java
+++ app/src/main/java/com/pvsagar/smartlockscreen/LockScreenActivity.java
@@ -1,7 +1,6 @@
 package com.pvsagar.smartlockscreen;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,8 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-
 
 
 public class LockScreenActivity extends Activity {
