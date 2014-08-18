70266953acfe2f923936caff0101ccdc9b5e4668
aravindsagar <aravindsuhruth@gmail.com>
2014-08-18 19:48:00 +0530

    Added a function header (for use by priyan)

--- app/src/main/java/com/pvsagar/smartlockscreen/applogic_helpers/EnvironmentHelper.java
+++ app/src/main/java/com/pvsagar/smartlockscreen/applogic_helpers/EnvironmentHelper.java
@@ -4,6 +4,10 @@ package com.pvsagar.smartlockscreen.applogic_helpers;
  * Created by aravind on 10/8/14.
  */
 public class EnvironmentHelper {
+    public void insertEnvironment(){
+
+    }
+
 
 
 }
