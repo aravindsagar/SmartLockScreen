223737273a9210224e53361d30d93805ba6b8b7b
Priyan Vaithilingam <priyanmuthu@gmail.com>
2014-09-13 22:48:49 +0530

    Added the most beautiful location search ever.

---
+++ app/src/main/java/com/pvsagar/smartlockscreen/SearchLocation.java
@@ -0,0 +1,201 @@
+package com.pvsagar.smartlockscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SearchLocation extends ActionBarActivity {
+
+    public static final int GEOCODER_MAX_RESULTS_COUNT = 5;
+    private final String LOG_TAG = SearchLocation.class.getSimpleName();
+
+    private static ArrayAdapter<String> addressAutoCompleteAdapter;
+    private static ArrayList<String> autoCompleteStrings;
+    private static List<Address> searchAddresses;
+    private static LocationSuggestTask locationSuggestTask;
+    private static EditText searchBarEditText;
+    private static Geocoder geocoder;
+
+    private static ListView searchLocationListView;
+
+    class LocationSuggestTask extends AsyncTask<String,Void,Void>{
+
+        @Override
+        protected Void doInBackground(String... params) {
+            if(autoCompleteStrings == null){
+                autoCompleteStrings = new ArrayList<String>();
+            } else {
+                autoCompleteStrings.clear();
+            }
+            try{
+                searchAddresses =  geocoder.getFromLocationName(params[0],GEOCODER_MAX_RESULTS_COUNT);
+                for (Address address : searchAddresses) {
+                    if(address.getFeatureName() != null){
+                        autoCompleteStrings.add(address.getFeatureName()+", "+address.getCountryName());
+                    } else {
+                        autoCompleteStrings.add(getReadableAddress(address));
+                    }
+                }
+                Log.v(LOG_TAG, "background work complete " + autoCompleteStrings.size());
+
+            } catch (Exception e){
+                Log.e(LOG_TAG, e.toString());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            addressAutoCompleteAdapter.clear();
+            Log.v(LOG_TAG,"On post execute started "+autoCompleteStrings.size());
+            for (String autoCompleteString : autoCompleteStrings) {
+                addressAutoCompleteAdapter.add(autoCompleteString);
+                //Log.v(LOG_TAG,autoCompleteString);
+            }
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_location);
+
+        /* Initialization */
+        init();
+        setUpActionBar();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.search_location, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setUpActionBar(){
+        /* Adding Contextual Action Bar with Done and Cancel Button */
+        final LayoutInflater layoutInflater = (LayoutInflater) getSupportActionBar().getThemedContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View customActionBarView = layoutInflater.inflate(R.layout.actionbar_custom_search, null);
+        customActionBarView.getBackground().setAlpha(100);
+        searchBarEditText = (EditText) customActionBarView.findViewById(R.id.edit_text_searchbar);
+        searchBarEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(locationSuggestTask != null && locationSuggestTask.getStatus() != AsyncTask.Status.FINISHED){
+                    locationSuggestTask.cancel(true);
+                }
+                locationSuggestTask = new LocationSuggestTask();
+                locationSuggestTask.execute(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        FrameLayout cancelButton = (FrameLayout)customActionBarView.findViewById(R.id.actionbar_cancel_icon);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(searchBarEditText.getText().toString().equals("")){
+                    onBackPressed();
+                }else {
+                    searchBarEditText.setText("");
+                }
+            }
+        });
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayOptions(
+                ActionBar.DISPLAY_SHOW_CUSTOM,
+                ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME
+                        | ActionBar.DISPLAY_SHOW_TITLE);
+        actionBar.setCustomView(customActionBarView,
+                new ActionBar.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+            /* End of Action Bar Code */
+    }
+
+    private void init(){
+        geocoder = new Geocoder(this);
+        autoCompleteStrings = new ArrayList<String>();
+        addressAutoCompleteAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,new ArrayList<String>());
+        searchLocationListView = (ListView)findViewById(R.id.list_view_location_search);
+        searchLocationListView.setAdapter(addressAutoCompleteAdapter);
+        searchLocationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent resultIntent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putDouble(SelectLocation.INTENT_EXTRA_SELECTED_LATITUDE,searchAddresses.get(position).getLatitude());
+                bundle.putDouble(SelectLocation.INTENT_EXTRA_SELECTED_LONGITUDE,searchAddresses.get(position).getLongitude());
+                resultIntent.putExtras(bundle);
+                Log.v(LOG_TAG,""+resultIntent.getExtras().getDouble(SelectLocation.INTENT_EXTRA_SELECTED_LATITUDE));
+                setResult(Activity.RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+
+    public static String getReadableAddress(Address address){
+        String readableAddress = "";
+        if(address.getFeatureName() != null){
+            readableAddress = address.getFeatureName();
+        }
+        for(int i=0;i<address.getMaxAddressLineIndex();i++){
+            if(!address.getAddressLine(i).equals("")) {
+                readableAddress = readableAddress + ", " + address.getAddressLine(i);
+            }
+        }
+        readableAddress = readableAddress+", "+address.getCountryName();
+        return readableAddress;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent resultIntent = new Intent();
+        setResult(RESULT_CANCELED,resultIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/pvsagar/smartlockscreen/SelectLocation.java
+++ app/src/main/java/com/pvsagar/smartlockscreen/SelectLocation.java
@@ -2,22 +2,14 @@ package com.pvsagar.smartlockscreen;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -30,9 +22,7 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 
 public class SelectLocation extends ActionBarActivity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
@@ -40,21 +30,15 @@ public class SelectLocation extends ActionBarActivity implements GooglePlayServi
     private final String LOG_TAG = SelectLocation.class.getSimpleName();
     public static final String INTENT_EXTRA_SELECTED_LATITUDE = "selectedLatitude";
     public static final String INTENT_EXTRA_SELECTED_LONGITUDE = "selectedLongitude";
-    public static final int GEOCODER_MAX_RESULTS_COUNT = 5;
-    public static final int AUTO_COMPLETE_THRESHOLD = 1;
+    public static final int REQUEST_SEARCH_LOCATION = 93;
+    private static final int MAP_DFAULT_ZOOM_LEVEL = 14;
+    private static final double DEFAULT_LATITUDE = 22;
+    private static final double DEFAULT_LONGITUDE = 75;
 
     private GoogleMap googleMap;
-    private static final int MAP_DFAULT_ZOOM_LEVEL = 14;
     private Location selectedLocation;
     private LocationClient mLocationClient;
 
-    //UI elements
-    private static Geocoder geocoder;
-    private static Button addressButton;
-    private static AutoCompleteTextView addressAutoCompleteTextView;
-    private static ArrayAdapter<String> addressAutoCompleteAdapter;
-    private static ArrayList<String> autoCompleteStrings;
-    private static LocationSuggestTask locationSuggestTask;
 
     //Intent
     Intent intent;
@@ -69,123 +53,29 @@ public class SelectLocation extends ActionBarActivity implements GooglePlayServi
         //Init
         setUpActionBar();
         setUpGoogleMap();
-
-        //Geocoder
-        locationSuggestTask = new LocationSuggestTask();
-        setUpGeocoder();
-
-
     }
 
-    class LocationSuggestTask extends AsyncTask<String, Void, Void> {
-        @Override
-        protected Void doInBackground(String... params) {
-            if(autoCompleteStrings == null){
-                autoCompleteStrings = new ArrayList<String>();
-            } else {
-                autoCompleteStrings.clear();
-            }
-            try{
-                List<Address> addresses =  geocoder.getFromLocationName(params[0].toString(),GEOCODER_MAX_RESULTS_COUNT);
-                addressAutoCompleteAdapter.clear();
-                for (Address address : addresses) {
-                    if(address.getFeatureName() != null){
-                        autoCompleteStrings.add(address.getFeatureName()+", "+address.getCountryName());
-                    } else {
-                        autoCompleteStrings.add(getReadableAddress(address));
-                    }
-                }
-
-            } catch (Exception e){
-                Log.e(LOG_TAG, e.toString());
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            addressAutoCompleteAdapter.clear();
-            for (String autoCompleteString : autoCompleteStrings) {
-                addressAutoCompleteAdapter.add(autoCompleteString);
-                Log.v(LOG_TAG,autoCompleteString);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_SEARCH_LOCATION){
+            if(resultCode == RESULT_OK){
+                Bundle bundle = data.getExtras();
+                double latitude = bundle.getDouble(INTENT_EXTRA_SELECTED_LATITUDE);
+                double longitude = bundle.getDouble(INTENT_EXTRA_SELECTED_LONGITUDE);
+                selectedLocation = new Location("Selected Location");
+                selectedLocation.setLatitude(latitude);
+                selectedLocation.setLongitude(longitude);
+                Log.v(LOG_TAG,"Activity success");
+                googleMap.clear();
+                googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude,longitude)));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude),MAP_DFAULT_ZOOM_LEVEL));
             }
         }
-    }
-
-    private void setUpGeocoder(){
-        geocoder = new Geocoder(this);
-        addressButton = (Button)findViewById(R.id.button_address);
-        addressAutoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.auto_complete_text_view_address);
-        autoCompleteStrings = new ArrayList<String>();
-        addressAutoCompleteAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,autoCompleteStrings);
-        addressAutoCompleteTextView.setAdapter(addressAutoCompleteAdapter);
-        addressAutoCompleteTextView.setThreshold(AUTO_COMPLETE_THRESHOLD);
-        addressAutoCompleteTextView.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                /*if(locationSuggestTask != null && !(locationSuggestTask.getStatus() == AsyncTask.Status.FINISHED)){
-                    locationSuggestTask.cancel(true);
-                }
-                locationSuggestTask = new LocationSuggestTask();
-                locationSuggestTask.execute(s.toString());*/
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                try{
-                    List<Address> addresses =  geocoder.getFromLocationName(s.toString(),GEOCODER_MAX_RESULTS_COUNT);
-                    addressAutoCompleteAdapter.clear();
-                    Toast.makeText(getBaseContext(),""+addresses.size(),Toast.LENGTH_SHORT).show();
-                    for (Address address : addresses) {
-                        addressAutoCompleteAdapter.add(getReadableAddress(address));
-                        Log.v(LOG_TAG,getReadableAddress(address));
-                    }
-                } catch (Exception e){
-                    Log.e(LOG_TAG, e.toString());
-                }
-            }
-        });
-        addressButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    for (Address address : geocoder.getFromLocationName(addressAutoCompleteTextView.getText().toString(),
-                            GEOCODER_MAX_RESULTS_COUNT)) {
-                        Log.v(LOG_TAG,address.toString());
-                    }
-                    ;
-
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, e.toString());
-                }
-            }
-        });
-    }
-
-    public String getReadableAddress(Address address){
-        String readableAddress = "";
-        if(address.getFeatureName() != null){
-            readableAddress = address.getFeatureName();
-        }
-        for(int i=0;i<address.getMaxAddressLineIndex();i++){
-            if(!address.getAddressLine(i).equals("")) {
-                readableAddress = readableAddress + ", " + address.getAddressLine(i);
-            }
-        }
-        readableAddress = readableAddress+", "+address.getCountryName();
-        return readableAddress;
     }
 
     private void setUpActionBar(){
         /* Adding Contextual Action Bar with Done and Cancel Button */
-        final LayoutInflater layoutInflater = (LayoutInflater) getActionBar().getThemedContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        final LayoutInflater layoutInflater = (LayoutInflater) getSupportActionBar().getThemedContext().getSystemService(LAYOUT_INFLATER_SERVICE);
         final View customActionBarView = layoutInflater.inflate(R.layout.actionbar_custom_view_done, null);
         customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
                 new View.OnClickListener() {
@@ -195,6 +85,17 @@ public class SelectLocation extends ActionBarActivity implements GooglePlayServi
                         onDoneButtonClick();
                     }
                 });
+
+        customActionBarView.findViewById(R.id.actionbar_search).setOnClickListener(
+                new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //On search button click
+                    Intent searchActivityIntent = new Intent(getBaseContext(),SearchLocation.class);
+                    searchActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivityForResult(searchActivityIntent, REQUEST_SEARCH_LOCATION);
+                }
+        });
 
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayOptions(
@@ -239,6 +140,7 @@ public class SelectLocation extends ActionBarActivity implements GooglePlayServi
         });
 
     }
+
 
     private void onDoneButtonClick(){
         if(selectedLocation != null){
@@ -286,24 +188,27 @@ public class SelectLocation extends ActionBarActivity implements GooglePlayServi
             latitude = intentBundle.getDouble(INTENT_EXTRA_SELECTED_LATITUDE, -1);
             longitude = intentBundle.getDouble(INTENT_EXTRA_SELECTED_LONGITUDE, -1);
         }
-        else{
-            latitude = -1;
-            longitude = -1;
-        }
-        if(latitude != -1 && longitude != -1){
-            selectedLocation = new Location("Selected Location");
-            selectedLocation.setLatitude(latitude);
-            selectedLocation.setLongitude(longitude);
-            LatLng latLng = new LatLng(latitude,longitude);
-            googleMap.addMarker(new MarkerOptions().position(latLng));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, MAP_DFAULT_ZOOM_LEVEL));
+        if(selectedLocation == null) {
+            if (latitude != -1 && longitude != -1) {
+                selectedLocation = new Location("Selected Location");
+                selectedLocation.setLatitude(latitude);
+                selectedLocation.setLongitude(longitude);
+                LatLng latLng = new LatLng(latitude, longitude);
+                googleMap.addMarker(new MarkerOptions().position(latLng));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, MAP_DFAULT_ZOOM_LEVEL));
 
-        } else {
-            selectedLocation = mLocationClient.getLastLocation();
-            LatLng latLng = new LatLng(selectedLocation.getLatitude(),selectedLocation.getLongitude());
-            googleMap.addMarker(new MarkerOptions().position(latLng));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, MAP_DFAULT_ZOOM_LEVEL));
-            mLocationClient.disconnect();
+            } else {
+                selectedLocation = mLocationClient.getLastLocation();
+                if (selectedLocation == null) {
+                    selectedLocation = new Location("Default location");
+                    selectedLocation.setLatitude(DEFAULT_LATITUDE);
+                    selectedLocation.setLongitude(DEFAULT_LONGITUDE);
+                }
+                LatLng latLng = new LatLng(selectedLocation.getLatitude(), selectedLocation.getLongitude());
+                googleMap.addMarker(new MarkerOptions().position(latLng));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, MAP_DFAULT_ZOOM_LEVEL));
+                mLocationClient.disconnect();
+            }
         }
 
     }
