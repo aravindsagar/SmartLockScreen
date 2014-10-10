746bf57c96ab4040cc9a39a7529179818a3e6c76
aravindsagar <aravindsuhruth@gmail.com>
2014-10-10 14:52:40 +0530

    Added a library for selecting and cropping images - will be useful for setting environment/user profile pictures

--- app/src/main/java/com/pvsagar/smartlockscreen/SmartLockScreenSettings.java
+++ app/src/main/java/com/pvsagar/smartlockscreen/SmartLockScreenSettings.java
@@ -34,7 +34,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class SmartLockScreenSettings extends ActionBarActivity
         implements SetMasterPasswordFragment.MasterPasswordSetListener,
         ManageEnvironmentFragment.ActionModeListener{
