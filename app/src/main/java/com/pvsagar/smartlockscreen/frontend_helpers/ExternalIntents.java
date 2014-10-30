9b0038452524addd7891d5ecc2fd750f54791ce5
aravindsagar <aravindsuhruth@gmail.com>
2014-10-30 15:40:57 +0530

    Bug fix: Opens normal camera when secure camera module is not available, and prevents crash if even normal camera app is unavailable.

--- app/src/main/java/com/pvsagar/smartlockscreen/frontend_helpers/ExternalIntents.java
+++ app/src/main/java/com/pvsagar/smartlockscreen/frontend_helpers/ExternalIntents.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.widget.Toast;
 
 /**
  * Created by PV on 10/25/2014.
@@ -17,13 +18,36 @@ public class ExternalIntents {
     }
 
     public static void launchCamera(Context context){
-        Intent intent;
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
+            try {
+                context.startActivity(getSecureCameraIntent());
+            } catch (Exception e){
+                try {
+                    context.startActivity(getNormalCameraIntent());
+                } catch (Exception e2){
+                    Toast.makeText(context, "Cannot start camera.", Toast.LENGTH_SHORT).show();
+                    e2.printStackTrace();
+                }
+            }
         } else {
-            intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+            try {
+                context.startActivity(getNormalCameraIntent());
+            } catch (Exception e2){
+                Toast.makeText(context, "Cannot start camera.", Toast.LENGTH_SHORT).show();
+                e2.printStackTrace();
+            }
         }
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
     }
+
+    private static Intent getSecureCameraIntent(){
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return intent;
+    }
+    private static Intent getNormalCameraIntent(){
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return intent;
+    }
+
 }
